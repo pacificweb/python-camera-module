169bbbaff1a3616c8b20b4036665c4b8c5b282af
Stephane Gagnon <stephane-gagnon@hotmail.com>
2021-06-07 23:20:09 -0400

    1st test prototype

--- eclipse/capture.py
+++ eclipse/capture.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import os
 import time
 import picamera
 
+cwd = os.path.dirname(os.path.abspath(__file__))
 with picamera.PiCamera(resolution=(1280, 720)) as camera:
     time.sleep(2)
     camera.start_preview()
-    camera.exposure = "night"
-    for filename in camera.capture_continuous('./gallery/img{counter:04d}.jpg'):
+    camera.exposure_mode = "night"
+    for filename in camera.capture_continuous(cwd+'/gallery/img{counter:04d}.jpg'):
         time.sleep(5)
