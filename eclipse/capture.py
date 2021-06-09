add5493b1edd992a461358a9b5bbb17b987c8b12
Stephane Gagnon <stephane-gagnon@hotmail.com>
2021-06-09 19:38:43 -0400

    Final version avant eclipse du 10 juin 2021

--- eclipse/capture.py
+++ eclipse/capture.py
@@ -7,11 +7,12 @@ import datetime as dt
 import picamera
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-with picamera.PiCamera(resolution=(1280, 720)) as camera:
+with picamera.PiCamera(resolution=(1296, 972)) as camera:
 
     camera.awb_mode = 'sunlight'
-    camera.annotate_background = picamera.Color('black')
-    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     time.sleep(2)
-    for filename in camera.capture_continuous(cwd+'/gallery/img{counter:04d}.jpg'):
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_text = 'eKlipse ' + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    for filename in camera.capture_continuous(cwd + '/gallery/img{counter:04d}.jpg'):
         time.sleep(5)
+        camera.annotate_text = 'eKlipse ' + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
