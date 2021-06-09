845600296e134fb6735c4b1a33e9efff98a959a5
Stephane Gagnon <stephane-gagnon@hotmail.com>
2021-06-08 21:25:33 -0400

    AWB mode test + annotations

--- eclipse/capture.py
+++ eclipse/capture.py
@@ -3,12 +3,15 @@
 
 import os
 import time
+import datetime as dt
 import picamera
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 with picamera.PiCamera(resolution=(1280, 720)) as camera:
+
+    camera.awb_mode = 'sunlight'
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     time.sleep(2)
-    camera.start_preview()
-    camera.exposure_mode = "night"
     for filename in camera.capture_continuous(cwd+'/gallery/img{counter:04d}.jpg'):
         time.sleep(5)
