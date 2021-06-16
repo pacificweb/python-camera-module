f6a8257029983f55ab63e765f4e76f7f8527a6bf
Stephane Gagnon <stephane-gagnon@hotmail.com>
2021-06-15 22:56:03 -0400

    Name refactoring

--- python-timelapse/capture.py
+++ python-timelapse/capture.py
@@ -9,10 +9,10 @@ import picamera
 cwd = os.path.dirname(os.path.abspath(__file__))
 with picamera.PiCamera(resolution=(1296, 972)) as camera:
 
-    camera.awb_mode = 'sunlight'
     time.sleep(2)
+    camera.exposure_mode = 'night'
     camera.annotate_background = picamera.Color('black')
-    camera.annotate_text = 'eKlipse ' + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     for filename in camera.capture_continuous(cwd + '/gallery/img{counter:04d}.jpg'):
-        time.sleep(5)
-        camera.annotate_text = 'eKlipse ' + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        time.sleep(12)
+        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
